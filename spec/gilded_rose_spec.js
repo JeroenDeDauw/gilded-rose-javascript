04fb360a9ee599a7a1ed44b4165a8ff8b09bc02a
jeroendedauw <jeroendedauw@gmail.com>
2015-05-29 16:01:01 +0200

    PP with DanielM

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,4 +4,20 @@ describe("Gilded Rose", function() {
     update_quality();
   });
 
+  it("should have 6 items in the global", function() {
+    expect(items.length).toBe(6);
+  });
+
+  it("should decrease sell_in by 1", function() {
+    var item = new Item('test item', 10, 20);
+    item.update_quality_and_sell_in();
+    expect(item.sell_in).toBe(9);
+  });
+
+  it("should decrease quality by two", function() {
+    var item = new Item('Conjured Mana Cake', 10, 20);
+    item.update_quality_and_sell_in();
+    expect(item.quality).toBe(18);
+  });
+
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,7 +4,69 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = []
+Item.prototype.update_quality_and_sell_in = function() {
+  this.update_quality();
+  this.update_sell_in();
+  this.update_special_quality();
+};
+
+Item.prototype.update_quality = function() {
+    if (this.name != 'Aged Brie' && this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.quality > 0) {
+        if (this.name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.name === 'Conjured Mana Cake') {
+            this.quality = this.quality - 2;
+          } else {
+            this.quality = this.quality - 1;
+          }
+        }
+      }
+    } else {
+      if (this.quality < 50) {
+        this.quality = this.quality + 1;
+        if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.sell_in < 11) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1;
+            }
+          }
+          if (this.sell_in < 6) {
+            if (this.quality < 50) {
+              this.quality = this.quality + 1;
+            }
+          }
+        }
+      }
+    }
+};
+
+Item.prototype.update_sell_in = function() {
+   if (this.name != 'Sulfuras, Hand of Ragnaros') {
+      this.sell_in = this.sell_in - 1;
+    }
+};
+
+Item.prototype.update_special_quality = function() {
+   if (this.sell_in < 0) {
+      if (this.name != 'Aged Brie') {
+        if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.quality > 0) {
+            if (this.name != 'Sulfuras, Hand of Ragnaros') {
+              this.quality = this.quality - 1;
+            }
+          }
+        } else {
+          this.quality = this.quality - this.quality;
+        }
+      } else {
+        if (this.quality < 50) {
+          this.quality = this.quality + 1;
+        }
+      }
+    }
+};
+
+var items = [];
 
 items.push(new Item('+5 Dexterity Vest', 10, 20));
 items.push(new Item('Aged Brie', 2, 0));
@@ -15,48 +77,8 @@ items.push(new Item('Conjured Mana Cake', 3, 6));
 
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
-    }
-  }
+   var item = items[i];
+
+    item.update_quality_and_sell_in();
+ }
 }
