709e2261e94ec45f47938395d5d09a334a707602
jeroendedauw <jeroendedauw@gmail.com>
2016-11-29 19:06:02 +0100

    PP with Bruno

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,15 @@
 describe("Gilded Rose", function() {
 
-  it("should do something", function() {
-    update_quality();
+  it("non-sulfuras should have its sell-in decreased by one", function() {
+    var item = {sell_in: 2, name: "something else", quality: 5};
+    update_item(item);
+    expect(item.sell_in).toBe(1);
+  });
+
+  it("sulfuras should not have sell-in decreased", function() {
+    var item = {sell_in: 2, name: "Sulfuras, Hand of Ragnaros", quality: 5};
+    update_item(item);
+    expect(item.sell_in).toBe(2);
   });
 
 });
