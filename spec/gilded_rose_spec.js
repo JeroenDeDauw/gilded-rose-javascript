48de597fbf48d2731b6c3304a76e9c64a0347b04
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-01-24 21:07:00 +0100

    PP with Thomas

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -4,4 +4,33 @@ describe("Gilded Rose", function() {
     update_quality();
   });
 
+	describe("Normal Item", function() {
+		it("should update a normal item correctly", function () {
+			var vest = new Item('+5 Dexterity Vest', 10, 20);
+			update_item(vest);
+			expect(vest.sell_in).toBe(9);
+			expect(vest.quality).toBe(19);
+		})
+
+		it("should update a normal item past sell_by date correctly", function () {
+			var vest = new Item('+5 Dexterity Vest', 0, 20); // TODO: verify this is correct with owner
+			update_item(vest);
+			expect(vest.quality).toBe(18);
+		})
+  })
+
+
+
+  it("should update a conjured item correctly", function () {
+      var conjuredItem = new Item('Conjured Mana Cake', 10, 20);
+      update_item(conjuredItem);
+      expect(conjuredItem.quality).toBe(18);
+  })
+
+  it("should update a conjured item past sell_by date correctly", function () {
+      var conjuredItem = new Item('Conjured Mana Cake', 0, 20); // TODO: verify this is correct with owner
+      update_item(conjuredItem);
+      expect(conjuredItem.quality).toBe(16);
+  })
+
 });
