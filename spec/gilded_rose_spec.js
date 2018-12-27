4134df27eb9ca4b6ea1197276619eb7a074bbd41
Jeroen De Dauw <jeroendedauw@gmail.com>
2018-12-27 16:35:15 +0100

    PP with Merel

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -2,6 +2,12 @@ describe("Gilded Rose", function() {
 
   it("should do something", function() {
     update_quality();
+    console.log(items)
+    expect(items[0].quality).toBe(19);
+    expect(items[0].sell_in).toBe(9);
+
+    expect(items[1].quality).toBe(1);
+    expect(items[1].sell_in).toBe(1);
   });
 
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,8 +13,18 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
+
+function update_normal_item(item){
+  if (item.quality > 0 ){
+    // TODO: check for quality === 1 and decreasing by 2
+    item.quality -= (item.sell_in < 0) ?  2 : 1;
+  }
+  item.sell_in -= 1;
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
+    // NORMAL CASE (no exepction)
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -22,6 +32,7 @@ function update_quality() {
         }
       }
     } else {
+      // AGED BRIE OR BACKSTAGE PASSES
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -38,6 +49,7 @@ function update_quality() {
         }
       }
     }
+
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
       items[i].sell_in = items[i].sell_in - 1;
     }
@@ -50,9 +62,10 @@ function update_quality() {
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = 0;
         }
       } else {
+        // Brie
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1
         }
